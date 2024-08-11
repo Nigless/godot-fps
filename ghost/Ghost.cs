a13430debb6ac99a0ba12800c10c29d8bf89f7d4
Nigless <nigless>
2024-08-11 15:23:01 +0000

    clean up

--- ghost/Ghost.cs
+++ ghost/Ghost.cs
@@ -5,75 +5,75 @@ using static Godot.GD;
 public partial class Ghost : CharacterBody3D
 {
 
-	public float Gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+    public float Gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
 
-	public StandingState StandingState;
-	public WalkingState WalkingState;
-	public FallingState FallingState;
-	public JumpingState JumpingState;
-	public RunningState RunningState;
-	public CrouchingState CrouchingState;
-	public CrouchingMovingState CrouchingMovingState;
-	public CrouchingFallingState CrouchingFallingState;
-	public CrouchingJumpingState CrouchingJumpingState;
+    public StandingState StandingState;
+    public WalkingState WalkingState;
+    public FallingState FallingState;
+    public JumpingState JumpingState;
+    public RunningState RunningState;
+    public CrouchingState CrouchingState;
+    public CrouchingMovingState CrouchingMovingState;
+    public CrouchingFallingState CrouchingFallingState;
+    public CrouchingJumpingState CrouchingJumpingState;
 
-	public State CurrentState;
-
-
-	[Export]
-	public Node3D Head;
-
-	[Export]
-	public CollisionShape3D Collider;
-
-	public override void _Ready()
-	{
-		Input.MouseMode = Input.MouseModeEnum.Captured;
-
-		StandingState = new StandingState(this);
-		WalkingState = new WalkingState(this);
-		FallingState = new FallingState(this);
-		JumpingState = new JumpingState(this);
-		RunningState = new RunningState(this);
-		CrouchingState = new CrouchingState(this);
-		CrouchingMovingState = new CrouchingMovingState(this);
-		CrouchingFallingState = new CrouchingFallingState(this);
-		CrouchingJumpingState = new CrouchingJumpingState(this);
-
-		CurrentState = StandingState;
-	}
-
-	public override void _PhysicsProcess(double delta)
-	{
-		UpdateState();
-		CurrentState.Update(delta);
-	}
+    public State CurrentState;
 
 
-	public override void _Input(InputEvent ev)
-	{
-		CurrentState.Input(ev);
-	}
+    [Export]
+    public Node3D Head;
+
+    [Export]
+    public CollisionShape3D Collider;
+
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+
+        StandingState = new StandingState(this);
+        WalkingState = new WalkingState(this);
+        FallingState = new FallingState(this);
+        JumpingState = new JumpingState(this);
+        RunningState = new RunningState(this);
+        CrouchingState = new CrouchingState(this);
+        CrouchingMovingState = new CrouchingMovingState(this);
+        CrouchingFallingState = new CrouchingFallingState(this);
+        CrouchingJumpingState = new CrouchingJumpingState(this);
+
+        CurrentState = StandingState;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        UpdateState();
+        CurrentState.Update(delta);
+    }
 
 
-	public void UpdateState()
-	{
+    public override void _Input(InputEvent ev)
+    {
+        CurrentState.Input(ev);
+    }
 
 
-		var state = CurrentState.UpdateState();
-
-		if (state == CurrentState)
-		{
-			return;
-		}
-
-		Print(state);
+    public void UpdateState()
+    {
 
 
-		CurrentState.Exit();
-		state.Enter();
-		CurrentState = state;
-	}
+        var state = CurrentState.UpdateState();
+
+        if (state == CurrentState)
+        {
+            return;
+        }
+
+        Print(state);
+
+
+        CurrentState.Exit();
+        state.Enter();
+        CurrentState = state;
+    }
 
 }
